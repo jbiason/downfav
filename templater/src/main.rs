6d4651d6548caa99b7137fb0b57fd5d843d74f89
Julio Biason <julio.biason@deliverit.com.br>
2019-02-21 18:45:14 -0300

    Starting with the comand line options

--- templater/src/main.rs
+++ templater/src/main.rs
@@ -1,3 +1,27 @@
+use clap::App;
+use clap::Arg;
+use clap::crate_name;
+use clap::crate_version;
+use clap::crate_authors;
+use clap::crate_description;
+
 fn main() {
+    let params = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("bind")
+            .short("b")
+            .long("bind")
+            .value_name("ADDRESS")
+            .help("Binding address for the service")
+            .takes_value(true))
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .value_name("PORT")
+            .help("Port to bind")
+            .takes_value(true))
+        .get_matches();
     println!("Hello, world!");
 }
