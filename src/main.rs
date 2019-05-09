6a7416a70ce52dfe1ef8465f36c331a891b3e0aa
Julio Biason <julio.biason@deliverit.com.br>
2019-05-09 09:01:01 -0300

    Skeleton for the monolith

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use elefren::prelude::*;
+use elefren::helpers::cli;
+
 fn main() {
-    println!("Hello, world!");
+    let registration = Registration::new("https://functional.cafe")
+        .client_name("downfav")
+        .build()
+        .unwrap();
+    let client = cli::authenticate(registration).unwrap();
+
+    println!("{:?}", client
+             .get_home_timeline().unwrap()
+             .items_iter()
+             .take(100)
+             .collect::<Vec<_>>()
+             );
 }
