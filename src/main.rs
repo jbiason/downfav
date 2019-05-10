438f37f77da67d772b228c77c14638ebca3fca48
Julio Biason <julio.biason@deliverit.com.br>
2019-05-10 12:50:27 -0300

    Save registration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use elefren::prelude::*;
 use elefren::helpers::cli;
+use elefren::helpers::toml;
 
 fn main() {
-    let registration = Registration::new("https://functional.cafe")
-        .client_name("downfav")
-        .build()
-        .unwrap();
-    let client = cli::authenticate(registration).unwrap();
+    let client = if let Ok(data) = toml::from_file("mastodon.toml") {
+        Mastodon::from(data)
+    } else {
+        let registration = Registration::new("https://functional.cafe")
+            .client_name("downfav")
+            .build()
+            .unwrap();
+        let mastodon = cli::authenticate(registration).unwrap();
+        toml::to_file(&*mastodon, "mastodon.toml").unwrap();
+        mastodon
+    };
 
     client
         .favourites().unwrap()
@@ -14,4 +21,12 @@ fn main() {
         .take(2)
         .for_each(move |record| println!("{:#?}", record))
         ;
+
+    // status
+    // status.account.acct (username)
+    // status.id (id)
+    // status.content
+    // status.media_attachments
+    //  -> attachment.remote_url / attachment.url
+    //     attachment.
 }
